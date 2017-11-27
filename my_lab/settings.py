662a9dd3acdebaf702079ae4abf8483b00b760c7
lyon <lozybean@163.com>
2017-11-27 10:54:11 +0800

    使用mysql

--- my_lab/settings.py
+++ my_lab/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'my_lab.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mylab',
+        'USER': 'liangzb',
+        'PASSWORD': 'lzb',
+        'HOST': '172.30.184.31',
+        'PORT': '3306',
     }
 }
 
