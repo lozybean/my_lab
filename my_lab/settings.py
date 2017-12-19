1737e462f0a4b3e2bc76d9cd2de73cb8d94f93dc
lyon <lozybean@163.com>
2017-12-19 17:33:09 +0800

    index显示以上机时为准

--- my_lab/settings.py
+++ my_lab/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&-gm0wwx4-g$nnhl9*m3x$)))m-m$_pdq811p4*!z-tv**q$bq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '172.30.184.31']
 
@@ -127,6 +127,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
     os.path.join(STATIC_ROOT, 'css'),
     os.path.join(STATIC_ROOT, 'js'),
 ]
